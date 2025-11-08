49261f6d3a0eabebff4b20462d7748b094b5b269
wojciechkaminski <wojciechk4minsk1@gamil.com>
2025-11-08 17:03:39 +0100

    lab 2.3 user added

--- src/main/java/pl/wsb/fitnesstracker/statistics/api/Statistics.java
+++ src/main/java/pl/wsb/fitnesstracker/statistics/api/Statistics.java
@@ -6,6 +6,7 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import pl.wsb.fitnesstracker.user.api.User;
 
 @Entity
 @Table(name = "Statistics")
@@ -18,6 +19,11 @@ public class Statistics {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Nullable
     private Long id;
+
+    // relacja 1-1 z użytkownikiem
+    @OneToOne
+    @JoinColumn(name = "user_id", nullable = false, unique = true)
+    private User user;
 
     @Column(name = "total_trainings", nullable = false)
     private int totalTrainings;
@@ -32,5 +38,6 @@ public class Statistics {
         this.totalTrainings = totalTrainings;
         this.totalDistance = totalDistance;
         this.totalCaloriesBurned = totalCaloriesBurned;
+        this.user = user;
     }
 }
