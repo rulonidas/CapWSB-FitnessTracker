daa71b9c4ec09180b5537c7bb2ac07ac2f04ecd7
krulewicz <rulewiczkacper@gmail.com>
2025-11-08 17:21:46 +0100

    versja lab 2.5

---
+++ src/main/java/pl/wsb/fitnesstracker/healthmetrics/Healthmetrics.java
@@ -0,0 +1,15 @@
+package pl.wsb.fitnesstracker.healthmetrics;
+
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+public class Healthmetrics {
+
+
+
+}
+
