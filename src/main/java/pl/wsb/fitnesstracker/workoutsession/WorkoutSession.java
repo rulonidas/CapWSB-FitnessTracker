2fabdf583475227a0b495c0a380734a34e50d8c6
wojciechkaminski <wojciechk4minsk1@gamil.com>
2025-11-22 17:28:08 +0100

    lab 3.9

--- src/main/java/pl/wsb/fitnesstracker/workoutsession/WorkoutSession.java
+++ src/main/java/pl/wsb/fitnesstracker/workoutsession/WorkoutSession.java
@@ -1,25 +1,52 @@
 package pl.wsb.fitnesstracker.workoutsession;
 
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import pl.wsb.fitnesstracker.training.api.Training;
 
-// TODO: Define the Event entity with appropriate fields and annotations
+import java.util.Date;
+
+@Entity
+@Table(name = "workout_session")
+@Getter
+
 public class WorkoutSession {
 
-    private final Training training;
     @Id
-    private int id;
-    private int trainingId;
-    private String timestamp;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @Transient
+    private Training training;
+
+
+    @Column(name = "training_id", nullable = false)
+    private Long trainingId;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "timestamp", nullable = false)
+    private Date timestamp;
+
+    @Column(name = "start_latitude")
     private double startLatitude;
+
+    @Column(name = "start_longitude")
     private double startLongitude;
+
+    @Column(name = "end_latitude")
     private double endLatitude;
+
+    @Column(name = "end_longitude")
     private double endLongitude;
+
+    @Column(name = "altitude")
     private double altitude;
 
     public WorkoutSession(
             Training training,
-            String timestamp,
+            Date timestamp,
             double startLatitude,
             double startLongitude,
             double endLatitude,
