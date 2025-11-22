3c829103600dec690a58b3eb8473bc2f29e6b9d4
wojciechkaminski <wojciechk4minsk1@gamil.com>
2025-11-22 17:23:41 +0100

    lab 3.6 workoutSeassion class

--- src/main/java/pl/wsb/fitnesstracker/training/api/Training.java
+++ src/main/java/pl/wsb/fitnesstracker/training/api/Training.java
@@ -1,29 +1,48 @@
 package pl.wsb.fitnesstracker.training.api;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import pl.wsb.fitnesstracker.training.internal.ActivityType;
 import pl.wsb.fitnesstracker.user.api.User;
 
 import java.util.Date;
+
 @Entity
 @Table(name = "trainings")
 @Getter
+
+
 public class Training {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+
+    @Transient
     private User user;
 
+
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "start_time", nullable = false)
     private Date startTime;
 
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "end_time", nullable = false)
     private Date endTime;
 
+    @Enumerated(EnumType.STRING)
+    @Column(name = "activity_type", nullable = false)
     private ActivityType activityType;
 
+    @Column(name = "distance", nullable = false)
     private double distance;
 
+    @Column(name = "average_speed", nullable = false)
     private double averageSpeed;
 
     public Training(
@@ -34,6 +53,7 @@ public class Training {
             final double distance,
             final double averageSpeed) {
         this.user = user;
+        this.userId = userId;
         this.startTime = startTime;
         this.endTime = endTime;
         this.activityType = activityType;
