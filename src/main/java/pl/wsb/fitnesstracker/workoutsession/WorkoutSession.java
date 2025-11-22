f4d2292a8c321b3102cd40c989e5b14ca3d32a89
wojciechkaminski <wojciechk4minsk1@gamil.com>
2025-11-22 16:46:24 +0100

    lab 3.3 wourkoutSeassion class

--- src/main/java/pl/wsb/fitnesstracker/workoutsession/WorkoutSession.java
+++ src/main/java/pl/wsb/fitnesstracker/workoutsession/WorkoutSession.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
+import pl.wsb.fitnesstracker.training.api.Training;
 
 @Entity
 @Table(name = "workout_session")
@@ -13,13 +14,37 @@ import lombok.Getter;
 // TODO: Define the Event entity with appropriate fields and annotations
 public class WorkoutSession {
 
+    private final Training training;
     @Id
     private int id;
+
     private int trainingId;
+
     private String timestamp;
+
     private double startLatitude;
+
     private double startLongitude;
+
     private double endLatitude;
+
     private double endLongitude;
+
     private double altitude;
+    public WorkoutSession(
+            Training training,
+            String timestamp,
+            double startLatitude,
+            double startLongitude,
+            double endLatitude,
+            double endLongitude,
+            double altitude) {
+        this.training = training;
+        this.timestamp = timestamp;
+        this.startLatitude = startLatitude;
+        this.startLongitude = startLongitude;
+        this.endLatitude = endLatitude;
+        this.endLongitude = endLongitude;
+        this.altitude = altitude;
+    }
 }
