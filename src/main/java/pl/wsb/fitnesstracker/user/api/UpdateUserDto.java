e2f6e77e0c599c6cbf5ef38e600bf65dbb4e7d19
krulewicz <rulewiczkacper@gmail.com>
2026-01-17 16:30:33 +0100

    lab 4.1

--- src/main/java/pl/wsb/fitnesstracker/user/api/UpdateUserDto.java
+++ src/main/java/pl/wsb/fitnesstracker/user/api/UpdateUserDto.java
@@ -1,9 +1,13 @@
 package pl.wsb.fitnesstracker.user.api;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import java.time.LocalDate;
 
 public record UpdateUserDto(
+
+        @JsonAlias({ "name", "first_name", "firstName" })
         String firstName,
+
         String lastName,
         String email,
         LocalDate birthdate
